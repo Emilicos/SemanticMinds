e1df13041fc1eb0c6b9048696f2e9c024ec5fc8a
AdrianRamadhan27 <ramadhanadrian2710@gmail.com>
2023-12-07 18:17:56 +0700

    Setup tailwind & Add basic navbar

--- frontend/views.py
+++ frontend/views.py
@@ -4,11 +4,13 @@ import requests
 
 # Create your views here.
 
-API_URL = "http://localhost:9999/api"
+API_URL = "http://localhost:8000/api"
 
 def searchPage(request):
     return render(request, 'home_page.html')
 
 def companyDetail(request, uri):
-    
-    return render(request, 'company_detail.html')
+    response = requests.get(f'{API_URL}/data/{uri}')
+    context = {'api_data': response.json()}
+
+    return render(request, 'company_detail.html', context)
