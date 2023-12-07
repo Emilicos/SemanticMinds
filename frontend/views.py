0bac2063a2182ba9cb845394029438f9ceb52885
AdrianRamadhan27 <ramadhanadrian2710@gmail.com>
2023-12-07 15:45:04 +0700

    Add frontend module

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+import requests
 
 # Create your views here.
+
+API_URL = "http://localhost:9999/api"
+
+def searchPage(request):
+    return render(request, 'home_page.html')
+
+def companyDetail(request, uri):
+    
+    return render(request, 'company_detail.html')
