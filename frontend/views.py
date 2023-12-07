6c56983810c7bde972a259a2ed0031a5c1290df3
emilicos <notalvaroaustin@gmail.com>
2023-12-08 05:33:44 +0700

    feat: finsihing up

--- frontend/views.py
+++ frontend/views.py
@@ -21,7 +21,7 @@ def searchpage(request):
     response = requests.get(f'{API_URL}/search/', params=params)
     context = {'api_data': response.json()['data'], 'pagination': response.json()['pagination'], 'range': range(1, response.json()['pagination']['last_page']+1), 'search': search}
     
-    print(context['api_data'])
+    print(context['pagination'])
     return render(request, 'search_page.html', context)
 
 def companyDetail(request, uri):
