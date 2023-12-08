15f73e108edcc4d66353b5ef91e77765348b7371
Kevin Alexander <kevinalexande26@gmail.com>
2023-12-08 10:19:50 +0700

    delete print output

--- frontend/views.py
+++ frontend/views.py
@@ -22,7 +22,7 @@ def searchpage(request):
     context = {'api_data': response.json()['data'], 'pagination': response.json()['pagination'], 'range': range(1, response.json()['pagination']['last_page']+1), 'search': search}
     
     # print(context['pagination'])
-    print(response.json())
+    # print(response.json())
     return render(request, 'search_page.html', context)
 
 def companyDetail(request, uri):
